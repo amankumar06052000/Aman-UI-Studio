0448c6e6af90c1058826e45ab9fc415f6d74523c
Aman Kumar <amankumar06052000@gmail.com>
2026-02-12 16:25:09 +0530

    Add files via upload

---
+++ send-email.php
@@ -0,0 +1,23 @@
+<?php
+// Form se data lena
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+// Aapki email jahan messages jaayenge
+$to = "aapkaemail@example.com"; // yahan apni email likho
+$subject = "New Contact Form Message";
+
+// Email body
+$body = "Name: $name\nEmail: $email\nMessage:\n$message";
+
+// Email headers
+$headers = "From: $email";
+
+// Email send
+if(mail($to, $subject, $body, $headers)){
+    echo "success"; // JS me check karenge
+} else {
+    echo "error";
+}
+?>
